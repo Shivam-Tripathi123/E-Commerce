b78417f5d4b0450db33077996858db436bc2949f
Shivam <shivamtripathi222002@gmail.com>
2023-11-10 13:48:48 +0530

    Routes and Controllers Setting is Done

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+
+
+
+//This woild encode the json data from request(just like urlencoded())
+app.use(express.json());
+
+
+//importing the route named product
+const product=require('./routes/productRoute');
+
+//using the route(product) in middleware for all api starting with "/api/v1". So "/api/v1" is common pefixes for all apis
+app.use('/api/v1',product);
+
+
+module.exports=app;
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const { getAllProducts } = require('../controller/productController');
+
+
+//this will help us to ue functionality like app.get() , app.post() in other file without importing express in app=express()  by just importing router in other files and doing router.get() router.post()
+const router=express.Router();
+//we are doing  router.get() instead of app.get() because we want a common prefix /product/ pr /user/.
+//This router must be connected with app/server.js in middleware after importing router. and after it we can access all url using router
+//at that middleware we can add a common prefixes eg app.use("user", router).   Now every api via route would already have "user" in it.
+
+
+//this means for api  with prefix "product"  and request "get" goto controller "getALLProduct"
+//previously all request with starting api "api/v1" is coming on this route() so the below line would goto getAllProducts controller on "/api/v1/products" 
+router.route('/products').get(getAllProducts);//eqvivalent to  "router.get('/products/' , getAllProducts)"
+//router.route('/products').get().post().put()      i.e. multiple request is handled in same line for same url with diff req method
+//so All method get chained using ".route(url)"
+
+module.exports=router;
